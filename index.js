2fcc4696f032188acf7d6b930f42483c3332216c
ezefranca <ezequiel.ifsp@gmail.com>
2016-09-20 19:10:36 -0300

    restore

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict'
 
+
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
@@ -15,7 +16,7 @@ app.use(bodyParser.json())
 
 // Index route
 app.get('/', function (req, res) {
-    res.send('Hello world, I am a chat bot')
+    res.send('olá amiguinho, tudo bem?')
 })
 
 // for Facebook verification
@@ -30,3 +31,37 @@ app.get('/webhook/', function (req, res) {
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
 })
+
+app.post('/webhook/', function (req, res) {
+    let messaging_events = req.body.entry[0].messaging
+    for (let i = 0; i < messaging_events.length; i++) {
+        let event = req.body.entry[0].messaging[i]
+        let sender = event.sender.id
+        if (event.message && event.message.text) {
+            let text = event.message.text
+            sendTextMessage(sender, "Voce me disse " + text.substring(0, 200) + " " + "...nao entendi, mas depois te ajudo a comprar um carro beleza? 👍 ")
+        }
+    }
+    res.sendStatus(200)
+})
+
+const token = process.env.FB_PAGE_ACCESS_TOKEN
+
+function sendTextMessage(sender, text) {
+    let messageData = { text:text }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
